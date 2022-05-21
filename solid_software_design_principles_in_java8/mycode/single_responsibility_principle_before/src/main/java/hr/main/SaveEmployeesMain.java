75108ec5acba3c7c21a8ec5ae8356943947437a3
Eric Ricketts <eric_ricketts@me.com>
2022-05-21 16:17:24 -0400

    single responsibility principle work

--- solid_software_design_principles_in_java8/mycode/single_responsibility_principle_before/src/main/java/hr/main/SaveEmployeesMain.java
+++ solid_software_design_principles_in_java8/mycode/single_responsibility_principle_before/src/main/java/hr/main/SaveEmployeesMain.java
@@ -1,19 +1,29 @@
 package hr.main;
 
+import hr.logging.ConsoleLogger;
+import hr.persistence.EmployeeFileSerializer;
 import hr.persistence.EmployeeRepository;
 import hr.personnel.Employee;
 
+import java.io.IOException;
 import java.util.List;
 
 public class SaveEmployeesMain {
     public static void main(String[] args) {
         // Grab employees
-        EmployeeRepository repository = new EmployeeRepository();
+        EmployeeFileSerializer employeeFileSerializer = new EmployeeFileSerializer();
+        EmployeeRepository repository = new EmployeeRepository(employeeFileSerializer);
+
+        ConsoleLogger consoleLogger = new ConsoleLogger();
         List<Employee> employees = repository.findAll();
 
-        // Save all
-        for (Employee e : employees){
-            Employee.save(e);
+        for (Employee employee : employees){
+            try {
+                repository.save(employee);
+                consoleLogger.writeInfo("Saved employee " + employee.toString());
+            } catch (IOException e) {
+                consoleLogger.writeError("Error saving employee ", e);
+            }
         }
     }
 }
